a92151d5f9762d8959e32ad8afc0069a435cbaba
Isaac Avilez <isaac1180@aol.com>
2019-11-06 16:09:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,7 +10,7 @@ def add_element_to_end_of_array(array, element)
   myArr = []
   myArr.push(array, element)
 end
-add_element_to_start_of_array("hello", "hey")
+
 def add_element_to_start_of_array(array, element)
   myArr = []
   myArr.unshift(array, element) 
@@ -39,3 +39,4 @@ end
 def update_element_from_index(array, index_number, element)
 
 end
+add_element_to_start_of_array("hello", "hey")
